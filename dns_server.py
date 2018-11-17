c61961fa5fa7aa607a9b5c0742e635b5c25288c5
Itay Feldman <itay1405@gmail.com>
2018-11-17 18:39:51 +0200

    temp
    Signed-off-by: Itay Feldman <itay1405@gmail.com>

---
+++ dns_server.py
@@ -0,0 +1,12 @@
+import socket
+
+# TODO go to http server project and add *if main*
+
+DNS_SERVER_IP = '0.0.0.0'
+DNS_SERVER_PORT = 53
+DEFAULT_BUFFER_SIZE = 1024
+
+
+def dns_udp_server(ip, port):
+    pass
+
